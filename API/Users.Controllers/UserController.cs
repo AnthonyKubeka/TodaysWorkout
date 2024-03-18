9007feccb27fd2846cb655561cf5707da5057467
= <nmkubeka@gmail.com>
2024-03-18 22:21:36 +0200

    startup api with Users Module

---
+++ API/Users.Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Users.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class UserController : ControllerBase
+    {
+        [HttpGet(Name = "GetUserByName/{name}")]
+        public ActionResult GetUserByName(string name) 
+        {
+            return Ok(name); 
+        }
+    }
+}
